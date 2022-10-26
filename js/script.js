de2d59b5a1cc7423b36ac0e7f5c304f854cf0e31
Vergefio <riccardods@live.it>
2022-10-26 16:01:13 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// **Consegna:**
+// Riprendiamo l’esercizio carosello e rifacciamolo, questa volta usando un array di oggetti.
+// Ogni elemento deve avere un titolo, una descrizione e il riferimento ad una immagine.
+
+//array di oggetti
+
+const carousel = [
+  {
+    img: "http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg"
+  },
+  {
+    img: "https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg"
+  },
+  {
+    img: "https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c"
+  },
+  {
+    img: "https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg"
+  },
+  {
+    img: "https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop"
+  },
+]
+
